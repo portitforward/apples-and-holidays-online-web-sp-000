276550b61043ae2628b79b9f27248013e2ee2b00
Cody Fussell <cody.fussell@gmail.com>
2019-09-27 00:14:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,14 +68,12 @@ def all_supplies_in_holidays(holiday_hash)
 		for holiday in holidays.keys
 			supplies = holidays[holiday]				
 			puts "  #{holiday.to_s.split("_").map{|h| h.capitalize}.join(" ")}: "  + supplies.map{|k| k}.join(", ")  
-			
 		end
 	end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  
 
 end
 
